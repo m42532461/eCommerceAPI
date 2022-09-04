5586f9ee6d495d463969f8187e336ab03e98a119
m42532461 <88074031+m42532461@users.noreply.github.com>
2022-09-04 13:29:57 +0800

    setup mongodb

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user.js");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DBConnection Successfully!"))
+  .catch((err) => console.log(err));
+
+app.use("/api/user", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is listening port ${process.env.PORT || 5000}`);
+});
