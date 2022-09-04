75548e9142afdca7f70174d1ae6dc78b628f7660
m42532461 <88074031+m42532461@users.noreply.github.com>
2022-09-04 20:29:55 +0800

    authentication and JWT

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user.js");
-
+const authRoute = require("./routes/auth.js");
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DBConnection Successfully!"))
   .catch((err) => console.log(err));
-
-app.use("/api/user", userRoute);
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is listening port ${process.env.PORT || 5000}`);
