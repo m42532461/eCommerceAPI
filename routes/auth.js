320cf4d018a3283080f6c73202213f5b54060b91
m42532461 <m42532461@gmail.com>
2022-09-12 15:35:56 +0800

    fix typo

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,7 @@ router.post("/register", async (req, res) => {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (error) {
-    res.status(500).json(err);
+    res.status(500).json(error);
   }
 });
 
@@ -49,7 +49,7 @@ router.post("/login", async (req, res) => {
     const { password, ...others } = user._doc;
     res.status(200).json({ ...others, accessToken });
   } catch (error) {
-    res.status(500).json(err);
+    res.status(500).json(error);
   }
 });
 
