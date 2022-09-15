a6f50547d743ec77d109032d891997bf542598c4
m42532461 <m42532461@gmail.com>
2022-09-15 16:40:25 +0800

    fix typo

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -27,7 +27,7 @@ const verifyTokenAndAuthorization = (req, res, next) => {
 
 const verifyTokenAndAdmin = (req, res, next) => {
   verifyToken(req, res, () => {
-    if (req.user.isAdmin) {
+    if (req.user?.isAdmin) {
       next();
     } else {
       res.status(403).json("You are not allow to do that!");
